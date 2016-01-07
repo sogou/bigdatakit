79bf519c80a39478b1e29edb3a5ed23d2be37bf5
Tao Li <litao@sogou-inc.com>
2016-01-07 20:21:11 +0800

    For table spec file, use human-friendly json instead of avro.

--- bigdatakit-hive/src/main/scala/com/sogou/bigdatakit/hive/CreateTable.scala
+++ bigdatakit-hive/src/main/scala/com/sogou/bigdatakit/hive/CreateTable.scala
@@ -4,60 +4,79 @@ import java.io.File
 
 import com.sogou.bigdatakit.spark.SparkSettings
 import com.typesafe.config.ConfigFactory
-import org.apache.avro.Schema
-import org.apache.spark.{SparkContext, SparkConf}
 import org.apache.spark.sql.hive.HiveContext
+import org.apache.spark.{SparkConf, SparkContext}
 import org.slf4j.LoggerFactory
 
 /**
   * Created by Tao Li on 2016/1/4.
   */
 object CreateTable {
-  val DEFAULT_DATABASE = "custom"
   val ORC_INPUTFORMAT = "org.apache.hadoop.hive.ql.io.orc.OrcInputFormat"
   val ORC_OUTPUTFORMAT = "org.apache.hadoop.hive.ql.io.orc.OrcOutputFormat"
   val ORC_SERDE = "org.apache.hadoop.hive.ql.io.orc.OrcSerde"
+
+  val SYMLINK_INPUTFORMAT = "com.sogou.datadir.plugin.SymlinkLzoTextInputFormat"
+  val HIVE_IGNOREKEY_OUTPUTFORMAT = "org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat"
+
+  import scala.collection.JavaConversions._
+
+  val DEFAULT_CONFIG = ConfigFactory.parseMap(Map("serde" -> "orc"))
 
   private val LOG = LoggerFactory.getLogger(getClass)
 
   def main(args: Array[String]) {
     if (args.length != 1) {
-      System.err.println("table avsc file is needed")
+      System.err.println("table spec file is needed")
       System.exit(1)
     }
 
-    val avscFile = args(0)
-    val schema = new Schema.Parser().parse(new File(avscFile))
+    val tableSpecFile = args(0)
+    val tableConfig = ConfigFactory.parseFile(new File(tableSpecFile)).withFallback(DEFAULT_CONFIG)
 
-    var database = schema.getNamespace
-    val table = schema.getName
-    var inputformat = schema.getProp("inputformat")
-    var outputformat = schema.getProp("outputformat")
-    var serde = schema.getProp("serde")
-    var location = schema.getProp("location")
+    val table = tableConfig.getString("table")
+    val serde = tableConfig.getString("serde")
+    val fields = tableConfig.getConfig("fields").entrySet().map { entry =>
+      val fieldName = entry.getKey
+      val fieldType = entry.getValue.atPath(fieldName).getString(fieldName)
+      (fieldName, fieldType)
+    }.toMap
 
-    if (database == null) database = DEFAULT_DATABASE
-    if (inputformat == null) inputformat = ORC_INPUTFORMAT
-    if (outputformat == null) outputformat = ORC_OUTPUTFORMAT
-    if (serde == null) serde = ORC_SERDE
-    if (location == null) location = s"/user/hive/warehouse/${database}.db/${table}"
-    val external = database match {
-      case "default" => "external"
-      case _ => ""
+    var database: String = null
+    var serdeClass: String = null
+    var inputFormatClass: String = null
+    var outputFormatClass: String = null
+    var isExternal: Boolean = false
+    var tblproperties: Map[String, String] = null
+
+    serde match {
+      case "orc" =>
+        database = "custom"
+        serdeClass = ORC_SERDE
+        inputFormatClass = ORC_INPUTFORMAT
+        outputFormatClass = ORC_OUTPUTFORMAT
+        isExternal = false
+        tblproperties = Map("orc.compress" -> "SNAPPY")
+      case _ =>
+        database = "default"
+        serdeClass = serde
+        inputFormatClass = SYMLINK_INPUTFORMAT
+        outputFormatClass = HIVE_IGNOREKEY_OUTPUTFORMAT
+        isExternal = true
+        tblproperties = Map()
     }
 
-    import scala.collection.JavaConversions._
+    val location = s"/user/hive/warehouse/${database}.db/${table}"
 
-    val prefixSql = s"create $external table $database.$table"
-    val fieldSql = schema.getFields().map(f =>
-      s"`${f.name()}` ${f.schema().getType.toString.toLowerCase}").mkString(", ")
+    val prefixSql = s"create ${if (isExternal) "external" else ""} table $database.$table"
+    val fieldSql = fields.map(field => s"`${field._1}` ${field._2}").mkString(", ")
     val partitionSql = "partitioned by(`logdate` string)"
-    val serdeSql = s"row format serde '$serde'"
-    val inputOutputFormatSql = s"stored as inputformat '$inputformat' outputformat '$outputformat'"
+    val serdeSql = s"row format serde '$serdeClass'"
+    val inputOutputFormatSql = s"stored as inputformat '$inputFormatClass' outputformat '$outputFormatClass'"
     val locationSql = s"location '$location'"
-    val propertySql = ""
+    val tblpropertiesSql = if (!tblproperties.isEmpty) s"tblproperties(${tblproperties.map(kv => s"'${kv._1}'='${kv._2}'").mkString(", ")})" else ""
 
-    val createTableSql = s"$prefixSql ($fieldSql) $partitionSql $serdeSql $inputOutputFormatSql $locationSql $propertySql"
+    val createTableSql = s"$prefixSql ($fieldSql) $partitionSql $serdeSql $inputOutputFormatSql $locationSql $tblpropertiesSql"
 
     LOG.info(s"create table sql: $createTableSql")
 
--- conf/bigdatakit-env.sh
+++ conf/bigdatakit-env.sh
@@ -3,5 +3,23 @@
 export JAVA_HOME=/usr/lib/jvm/java
 
 export SPARK_HOME=/opt/spark
+export SPARK_CONF_DIR=$SPARK_HOME/conf
+export SPARK_LIB_DIR=$SPARK_HOME/lib
+export SPARK_ASSEMBLY_JAR=$SPARK_LIB_DIR/"$(ls -1 "$SPARK_LIB_DIR" | grep "^spark-assembly.*hadoop.*\.jar$" || true)"
+. $SPARK_CONF_DIR/spark-env.sh
 
-# export BIGDATAKIT_CLASSPATH=
+export HIVE_HOME=/opt/datadir
+export HIVE_CONF_DIR=$HIVE_HOME/conf
+export HIVE_AUXLIB_DIR=$HIVE_HOME/auxlib
+for jar in $HIVE_AUXLIB_DIR/*.jar; do
+  HIVE_AUXLIB_CLASSPATH=$HIVE_AUXLIB_CLASSPATH:$jar
+done
+export HIVE_AUXLIB_CLASSPATH=$HIVE_AUXLIB_CLASSPATH
+
+export BIGDATAKIT_HOME=/opt/bigdatakit
+export BIGDATAKIT_CONF_DIR=$BIGDATAKIT_HOME/conf
+export BIGDATAKIT_LIB_DIR=$BIGDATAKIT_HOME/lib
+for jar in $BIGDATAKIT_LIB_DIR/*.jar; do
+  BIGDATAKIT_CLASSPATH=$BIGDATAKIT_CLASSPATH:$jar
+done
+export BIGDATAKIT_CLASSPATH=$BIGDATAKIT_CLASSPATH
