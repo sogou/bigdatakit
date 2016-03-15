ae8e2081e0cedd1f7158f0b9fd4e09ca0d234c8d
Tao Li <litao@sogou-inc.com>
2016-03-15 20:54:08 +0800

    Update AvroUtils.

--- bigdatakit-common/src/main/scala/com/sogou/bigdatakit/common/util/AvroUtils.java
+++ bigdatakit-common/src/main/scala/com/sogou/bigdatakit/common/util/AvroUtils.java
@@ -3,8 +3,8 @@ package com.sogou.bigdatakit.common.util;
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericDatumReader;
 import org.apache.avro.generic.GenericDatumWriter;
-import org.apache.avro.generic.GenericRecord;
 import org.apache.avro.io.*;
+import org.apache.avro.specific.SpecificRecordBase;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -13,10 +13,12 @@ import java.io.IOException;
  * Created by Tao Li on 2016/3/15.
  */
 public class AvroUtils {
-  public static <T extends GenericRecord> byte[] avroObjectToBytes(T object) throws IOException {
+  public static <T extends SpecificRecordBase> byte[] avroObjectToBytes(T object)
+      throws IOException {
     ByteArrayOutputStream output = new ByteArrayOutputStream();
     BinaryEncoder encoder = EncoderFactory.get().binaryEncoder(output, null);
-    DatumWriter<GenericRecord> writer = new GenericDatumWriter<GenericRecord>(object.getSchema());
+    DatumWriter<SpecificRecordBase> writer =
+        new GenericDatumWriter<SpecificRecordBase>(object.getSchema());
     try {
       writer.write(object, encoder);
       encoder.flush();
@@ -26,8 +28,9 @@ public class AvroUtils {
     }
   }
 
-  public static <T extends GenericRecord> T avroBytesToObject(byte[] bytes, Schema schema) throws IOException {
-    DatumReader<GenericRecord> reader = new GenericDatumReader<GenericRecord>(schema);
+  public static <T extends SpecificRecordBase> T avroBytesToObject(byte[] bytes, Schema schema)
+      throws IOException {
+    DatumReader<SpecificRecordBase> reader = new GenericDatumReader<SpecificRecordBase>(schema);
     BinaryDecoder decoder = DecoderFactory.get().binaryDecoder(bytes, null);
     return (T) reader.read(null, decoder);
   }
