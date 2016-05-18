efe1db91252ed50e7c98d72e45c0b7dd04867fc7
Tao Li <litao@sogou-inc.com>
2016-05-18 11:31:22 +0800

    Update JDBCConnectionPool.

--- bigdatakit-common/src/main/java/com/sogou/bigdatakit/common/jdbc/JDBCConnectionPool.java
+++ bigdatakit-common/src/main/java/com/sogou/bigdatakit/common/jdbc/JDBCConnectionPool.java
@@ -5,26 +5,37 @@ import com.sogou.bigdatakit.common.db.ConnectionPoolException;
 
 import java.io.IOException;
 import java.sql.*;
+import java.util.Properties;
 
 /**
  * Created by Tao Li on 4/11/16.
  */
 public class JDBCConnectionPool extends ConnectionPool<Connection> {
   private String url;
+  private Properties info;
 
   public JDBCConnectionPool(String driver, String url) throws ConnectionPoolException {
+    this(driver, url, null);
+  }
+
+  public JDBCConnectionPool(String driver, String url, Properties info) throws ConnectionPoolException {
     try {
       Class.forName(driver);
     } catch (ClassNotFoundException e) {
       throw new ConnectionPoolException(e);
     }
     this.url = url;
+    this.info = info;
   }
 
   @Override
   protected Connection createConnection() throws IOException {
     try {
-      return DriverManager.getConnection(url);
+      if (info == null) {
+        return DriverManager.getConnection(url);
+      } else {
+        return DriverManager.getConnection(url, info);
+      }
     } catch (SQLException e) {
       throw new IOException(e);
     }
